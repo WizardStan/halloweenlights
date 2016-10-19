6c3f073123c9d27125c162bd14c90e2fbc26d755
Wizard Stan <wizard@houseoffire.ca>
2016-10-18 21:52:30 -0400

    Single LED stares for 10-30 seconds then blinks twice in rapid succession

--- main.c
+++ main.c
@@ -1,29 +1,62 @@
 #include <pic18fregs.h>
 #include <stdint.h>
 
+#include <delay.h>
+
 #pragma config XINST=OFF
+
 #pragma config FOSC = INTOSCIO_EC
+
 #pragma config WDT = OFF
+
 #pragma config LVP = OFF
 
 #define LED_LAT LATCbits.LATC0
 #define LED_TRIS TRISCbits.TRISC0
 
+uint16_t randDelay = 20000;
+uint16_t getDelay() {
+	randDelay ^= randDelay >> 3;
+	randDelay ^= randDelay << 6;
+	randDelay ^= randDelay >> 7;
+	randDelay >>= 1;
+	randDelay += 20000;
+	return randDelay;
+}
+
+// concept: Long period LED on, rapid succession off-on-on, then back to start
+// How long is the period?  Pseudo random, between 10 and 20 seconds
+typedef enum {
+	STARE,
+	BLINK_ONE,
+	GLANCE,
+	BLINK_TWO
+} BlinkState;
+
+uint16_t countDown;
+BlinkState state;
+
 void main(void)
 {
-	uint16_t blink = 0;
+	countDown = getDelay();
+	state = STARE;
 
 	LED_TRIS = 0; // Pin as output
 	LED_LAT = 0; // LED off
-	blink = 0;
 	while (1) {
-		if (blink < 1000) {
+		if (state == STARE || state == GLANCE) {
 			LED_LAT = !LED_LAT; // Toggle LED
-		} else if (blink == 1000) {
-			LED_LAT = 0;
-		} else if (blink > 2000) {
-			blink = 0;
 		}
-		++blink;
+		--countDown;
+		if (countDown == 0) {
+			++state;
+			if (state > BLINK_TWO) {
+				state = STARE;
+				countDown = getDelay();
+			} else {
+				countDown = 300;
+			}
+			LED_LAT = !(state & 0x1);
+		}
 	}
 }
